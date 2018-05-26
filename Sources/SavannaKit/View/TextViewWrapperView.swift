4efffe7e945445c3b5d8c868e9db905f7bba475e
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-05-26 21:51:53 +0200

    Fix gutter on macOS

--- Sources/SavannaKit/View/TextViewWrapperView.swift
+++ Sources/SavannaKit/View/TextViewWrapperView.swift
@@ -52,46 +52,53 @@ import Foundation
 				return
 			}
 			
-			guard let lineNumbersStyle = theme.lineNumbersStyle else {
+			if theme.lineNumbersStyle == nil {
+
 				textView.hideGutter()
-				return
-			}
-			
-			let contentHeight = textView.enclosingScrollView!.documentView!.bounds.height
-			
-			let yOffset = self.bounds.height - contentHeight
-			
-			var paragraphs: [Paragraph]
-			
-			if let cached = textView.cachedParagraphs {
 				
-				paragraphs = cached
+				let gutterRect = CGRect(x: 0, y: rect.minY, width: textView.gutterWidth, height: rect.height)
+				let path = BezierPath(rect: gutterRect)
+				path.fill()
 				
 			} else {
-				
-				paragraphs = generateParagraphs(for: textView, flipRects: true)
-				textView.cachedParagraphs = paragraphs
-				
+			
+				let contentHeight = textView.enclosingScrollView!.documentView!.bounds.height
+			
+				let yOffset = self.bounds.height - contentHeight
+			
+				var paragraphs: [Paragraph]
+			
+				if let cached = textView.cachedParagraphs {
+					
+					paragraphs = cached
+					
+				} else {
+					
+					paragraphs = generateParagraphs(for: textView, flipRects: true)
+					textView.cachedParagraphs = paragraphs
+					
+				}
+			
+				paragraphs = offsetParagraphs(paragraphs, for: textView, yOffset: yOffset)
+			
+				let components = textView.text.components(separatedBy: .newlines)
+			
+				let count = components.count
+			
+				let maxNumberOfDigits = "\(count)".count
+			
+				textView.updateGutterWidth(for: maxNumberOfDigits)
+			
+				theme.gutterStyle.backgroundColor.setFill()
+			
+				let gutterRect = CGRect(x: 0, y: 0, width: textView.gutterWidth, height: rect.height)
+				let path = BezierPath(rect: gutterRect)
+				path.fill()
+			
+			
+				drawLineNumbers(paragraphs, in: self.bounds, for: textView)
+			
 			}
-			
-			paragraphs = offsetParagraphs(paragraphs, for: textView, yOffset: yOffset)
-			
-			let components = textView.text.components(separatedBy: .newlines)
-			
-			let count = components.count
-			
-			let maxNumberOfDigits = "\(count)".count
-			
-			textView.updateGutterWidth(for: maxNumberOfDigits)
-			
-			theme.gutterStyle.backgroundColor.setFill()
-			
-			let gutterRect = CGRect(x: 0, y: 0, width: textView.gutterWidth, height: rect.height)
-			let path = BezierPath(rect: gutterRect)
-			path.fill()
-			
-			
-			drawLineNumbers(paragraphs, in: self.bounds, for: textView)
 			
 		}
 		
